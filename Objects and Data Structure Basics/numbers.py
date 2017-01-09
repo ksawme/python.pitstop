0ebed6a148d1ffbf63b5d9fa83367bd2ea402a19
ksawme <ksawme@Sams-MacBook-Pro.local>
2017-01-10 00:19:39 +0530

    Adding numbers.py

---
+++ Objects and Data Structure Basics/numbers.py
@@ -0,0 +1,9 @@
+#Below line is used to tell the Python Compiler to use Python 3.6 version
+#!/usr/bin/env python3.6
+import sys
+#Demonstration of Numbers
+print(2 + 1)
+print(2 - 1)
+print(2 * 2)
+print(3 / 2)
+print(sys.version)
