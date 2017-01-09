738835926206ba3f511ed64ec2076d7a6be07bc9
ksawme <ksawme@Sams-MacBook-Pro.local>
2017-01-10 01:32:45 +0530

    Added string commands file

---
+++ Objects and Data Structure Basics/strings.py
@@ -0,0 +1,48 @@
+from __future__ import division
+print('Hello')
+print('This is also a string')
+print("This is a string")
+print("I'm a string")
+print('"this is a quote"')
+print('Hello World 1')
+print('Hello World 2')
+
+print 'Hey'
+print('Here is a new line \n & here is the second line')
+print('Here is a new line \t & here is the second line')
+
+print(len('Hello World'))
+
+s = 'Hello World'
+print(s)
+print(s[0])
+print(s[1])
+
+print(s[1:])
+print(s[:3])
+print(s[1:3])
+print(s[:])
+print(s[-1])
+print(s[:-1])
+print(s[::1])
+print(s[::2])
+
+#Reverse the string
+print(s[::-1])
+
+print(s + ' concatenate me')
+
+s = s + ' concatenate me!'
+print(s)
+
+letter = 'z'
+print(letter)
+letter = letter * 10
+print(letter)
+
+s = 'Hello'
+
+print(s.upper())
+print(s.lower())
+print(s.split())
+print(s.split('e'))
